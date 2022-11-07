07a1aae5460629af229b431dc75f65d27c25f0b0
jcadafalch <84527588+jcadafalch@users.noreply.github.com>
2022-11-07 11:16:53 +0100

    change dark mode running

--- GestorTareas/Client/Shared/AppBar.razor.cs
+++ GestorTareas/Client/Shared/AppBar.razor.cs
@@ -5,40 +5,19 @@ namespace GestorTareas.Client.Shared
 {
     public partial class AppBar
     {
-        private bool _isLightMode = true;
-        private MudTheme _currentTheme = new MudTheme();
+        private bool _isDarkMode ;
 
         [Parameter]
         public EventCallback OnSidebarToggled { get; set; }
+
         [Parameter]
-        public EventCallback<MudTheme> OnThemeToggled { get; set; }
+        public EventCallback<bool> OnThemeToggled { get; set; }
 
         private async Task ToggleTheme()
         {
-            _isLightMode = !_isLightMode;
+            _isDarkMode = !_isDarkMode;
 
-            _currentTheme = !_isLightMode ? GenerateDarkTheme() : new MudTheme();
-
-            await OnThemeToggled.InvokeAsync(_currentTheme);
+            await OnThemeToggled.InvokeAsync(_isDarkMode);
         }
-
-        private MudTheme GenerateDarkTheme() =>
-            new MudTheme
-            {
-                Palette = new Palette()
-                {
-                    Black = "#27272f",
-                    Background = "#32333d",
-                    BackgroundGrey = "#27272f",
-                    Surface = "#373740",
-                    TextPrimary = "#ffffffb3",
-                    TextSecondary = "rgba(255,255,255, 0.50)",
-                    AppbarBackground = "#27272f",
-                    AppbarText = "#ffffffb3",
-                    DrawerBackground = "#27272f",
-                    DrawerText = "#ffffffb3",
-                    DrawerIcon = "#ffffffb3"
-                }
-            };
     }
 }
