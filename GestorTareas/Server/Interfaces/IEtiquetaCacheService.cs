45da6c07ece90b6f1ab9b2eefc0bd761cd40153a
jcadafalch <84527588+jcadafalch@users.noreply.github.com>
2022-11-28 10:21:00 +0100

    solved problems to close issue #58

--- GestorTareas/Server/Interfaces/IEtiquetaCacheService.cs
+++ GestorTareas/Server/Interfaces/IEtiquetaCacheService.cs
@@ -8,20 +8,20 @@ public interface IEtiquetaCacheService
     /// Obtiene el listado de etiquetas de cache
     /// </summary>
     /// <returns>Listado de etiquetas en cache</returns>
-    List<Etiqueta>? Get();
+    IEnumerable<Etiqueta>? Get();
 
     /// <summary>
     /// Inserta o actualiza valores en cache
     /// </summary>
     /// <param name="value">Valor que se va a almacenar</param>
-    void Upsert(List<Etiqueta> value);
+    void Upsert(IEnumerable<Etiqueta> value);
 
     /// <summary>
     /// Inserta o actualiza valores en cache
     /// </summary>
     /// <param name="value">Valor que se va a almacenar</param>
     /// <param name="expiration">Tiempo que transcurre antes de que los valores se eliminen</param>
-    void Upsert(List<Etiqueta> value, TimeSpan expiration);
+    void Upsert(IEnumerable<Etiqueta> value, TimeSpan expiration);
 
     /// <summary>
     /// Elimina de caché el contenido
